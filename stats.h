5097203893c2c99d2d5f40fe6cd90077ff7eca10
Zoe Wood <zwood@calpoly.edu>
2021-02-16 21:55:55 -0800

    Add files via upload

--- stats.h
+++ stats.h
@@ -3,13 +3,14 @@
 
 #include <vector> 
 
-/* tool for computing various statistic */
 class stats {
- public:
+public:
 	static double computeMean(std::vector<double> theNums);
 	static double computePopMean(std::vector<double> theNums, int totPop);
 	static double computeStdDevSample(std::vector<double> theNums);
-	static double computeStdDevPop(std::vector<double> theNums);
+	static double computeStdDevPop(std::vector<double> theNums, double theMean);
 	static double computeCorCoeff(std::vector<double> inX, std::vector <double> inY);
+	static double computeCorCoeffPop(std::vector<double> inX, std::vector <double> inY, double mX, double mY);
+	static double computeChiSqGoodFit(std::vector<double> obs, std::vector <double> exp);
 };
 #endif
